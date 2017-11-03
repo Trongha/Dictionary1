e590cf998784a366a7d2ac4487f2ec501e8a3273
SolumateFriend <Trongha.hd.98@gmail.com>
2017-11-03 12:11:07 +0700

    sap xong search

---
+++ src/Generall.java
@@ -0,0 +1,19 @@
+import data.Word;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+public class General {
+    public static String[] getKeyOfHashMap(HashMap<String, Word> map){
+        String[] keyArray = new String[map.size()];
+
+        Set<String> set = map.keySet();
+        Iterator i = set.iterator();
+        int i_key =0;
+        while (i.hasNext()){
+            keyArray[i_key++] = i.
+        }
+    }
+}
